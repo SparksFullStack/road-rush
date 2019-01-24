aa5c93749c3336262c8a00185644ba2bd1f1e0d5
SparksFullStack <csparks@antioch.edu>
2019-01-24 15:01:53 -0800

    car responding to collisions by modifying its alpha value

--- js/classes/utilities/collision.js
+++ js/classes/utilities/collision.js
@@ -1,7 +1,7 @@
 class Collision {
     static checkColide(obj1, obj2){
         // here we'll subtract the x of object 1 from the x of object 2 and use the abs method to remove the negative sign if present
-        const distX = Math.abs(ob1.x - obj2.x);
+        const distX = Math.abs(obj1.x - obj2.x);
         const distY = Math.abs(obj1.y - obj2.y);
 
         // if the distance is less than half the width AND height of the object, that means that they have collided
