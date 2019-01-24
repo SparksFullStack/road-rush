3b1c950d32937ffdd14f5e3e1e6c61e1b6f36991
SparksFullStack <csparks@antioch.edu>
2019-01-23 16:19:15 -0800

    basic file structure added

---
+++ js/SceneMain.js
@@ -0,0 +1,19 @@
+class SceneMain extends Phaser.Scene {
+    constructor() {
+        super('SceneMain');
+    }
+    preload()
+    {
+        // preload all assets
+    }
+    create() {
+        // defines all objects for the scene        
+    }
+    update() {
+        // constantly runnning loop
+    }
+
+    // *NOTE: you can add custom functions, as well
+}
+
+const newScene = new SceneMain();
